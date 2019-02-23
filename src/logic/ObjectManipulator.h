01d86232f638315fbcff4440551d864aca0f9f16
MAGANER <maganer.krik@mail.ru>
2019-02-23 21:36:56 +0300

    added tile cutting after entering all info

--- src/logic/ObjectManipulator.h
+++ src/logic/ObjectManipulator.h
@@ -6,18 +6,23 @@
 #include"gui/object_windows/ObjectCreatingMenu.h"
 #include"gui/object_windows/ObjectParameterWindow.h"
 #include"gui/object_windows/Moving_length_menu.h"
+#include"gui/object_windows/TileCuttingWindow.h"
+#include"logic/TileCutter.h"
 
 class ObjectManipulator
 {
 private:
 	int moving_length;
 	int current_object_id;
+
+    void create(vector<Entity *> & objects);
+    void cut_tile(vector<Entity *> & objects);
+    void set_new_obj_near_to_last(vector<Entity *> & objects);
 public:
 	void create_obj(vector<Entity *> & objects);
 	void delete_obj(vector<Entity *> & objects);
 	void move_obj(vector<Entity *> & objects,string direction);
 	void rotate_obj(vector<Entity *> & objects,float angle);
-	void change_obj_parameters(vector<Entity *> & objects, int & object_id);
 	void change_obj_parameters(vector<Entity *> & objects);
 	void choose_obj(int id);
 	void change_moving_length();
